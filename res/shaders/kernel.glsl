06aafe005978b487c60f326c1467d136ee2c2862
Michael Braun <michael.braun1994@gmail.com>
2023-01-15 14:56:14 +0100

    normalized distance in kernel

--- res/shaders/kernel.glsl
+++ res/shaders/kernel.glsl
@@ -3,12 +3,13 @@
 
 float kernel(float distance, float smoothing_window) {
     float ret_val = 0.0;
+    float normed_distance = distance / smoothing_window;
 
-    if (distance <= 0.5) {
-        ret_val = 6.0 * (pow(distance, 3.0) - pow(distance, 2.0)) + 1.0;
+    if (normed_distance <= 0.5) {
+        ret_val = 6.0 * (pow(normed_distance, 3.0) - pow(normed_distance, 2.0)) + 1.0;
     } 
-    else if (distance <= 1.0) {
-        ret_val = 2.0 * pow((1.0 - distance), 3.0);
+    else if (normed_distance <= 1.0) {
+        ret_val = 2.0 * pow((1.0 - normed_distance), 3.0);
     }
 
     return (1.0 / (PI * pow(smoothing_window, 3.0))) * ret_val;
@@ -16,7 +17,7 @@ float kernel(float distance, float smoothing_window) {
 
 vec3 kernelGradient(vec3 distance_vec, float smoothing_window) {
     float scalar = 0.0;
-    float r = length(distance_vec);
+    float r = length(distance_vec) / smoothing_window;
 
     if(r < EPS){
         return vec3(0);
